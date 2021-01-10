5f0ec4e6a9c52f4083ed3614a3fa9a58155ffe2a
Stefan <stefan.gulas@gmail.com>
2021-01-10 19:38:25 +0100

    Projektdateien hinzufügen.

---
+++ DataStructures/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+  public class Stack
+  {
+    
+    private List<string> stackList { get; set; }
+    public List<string> StackList { get; set; }
+    public List<string> FillStack(string newItem)
+    {
+      stackList.Add(newItem)
+    }
+    public List<string> EmptyStack()
+    {
+      var firstItem = stackList[0];
+      stackList.Remove(firstItem);
+    }
+  }
+}
