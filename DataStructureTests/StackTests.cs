dde95981beed79800b6e58c3f710008431db5406
Stefan <stefan.gulas@gmail.com>
2021-01-10 20:50:10 +0100

    First Stack test working

---
+++ DataStructureTests/StackTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using DataStructures;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructureTests
+{
+  public class StackTests
+  {
+    private StackNew stackNew;
+
+    public StackNew StackNew { get => stackNew; set => stackNew = value; }
+
+    [SetUp]
+    public void Setup()
+    {
+      stackNew = new StackNew();
+      stackNew.FillStack("firstItem");
+      stackNew.FillStack("secondItem");
+
+    }
+
+    [Test]
+    public void TestTwoItemsIsEqual()
+    {
+      var expectedStack = new List<string>();
+      expectedStack.Add("firstItem");
+      expectedStack.Add("secondItem");
+      var actualStack = stackNew.GetStack();
+      Assert.IsTrue(actualStack.SequenceEqual(expectedStack));
+    }
+    [Test]
+    public void TestRemoveOneItemIsTrue()
+    {
+      var expectedStack = new List<string>();
+      expectedStack.Add("firstItem");
+      
+      var actualStack = stackNew.EmptyStack();
+      Assert.IsTrue(actualStack.SequenceEqual(expectedStack));
+    }
+  }
+}
--- DataStructures/StackNew.cs
+++ DataStructures/StackNew.cs
@@ -4,19 +4,28 @@ using System.Text;
 
 namespace DataStructures
 {
-  public class Stack
+  public class StackNew
   {
-    
+    public StackNew()
+    {
+      stackList = new List<string>();
+    }
     private List<string> stackList { get; set; }
     public List<string> StackList { get; set; }
     public List<string> FillStack(string newItem)
     {
-      stackList.Add(newItem)
+      stackList.Add(newItem);
+      return stackList;
     }
     public List<string> EmptyStack()
     {
       var firstItem = stackList[0];
       stackList.Remove(firstItem);
+      return stackList;
+    }
+    public List<string> GetStack()
+    {
+      return stackList;
     }
   }
 }
