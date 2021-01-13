9b2280e4c9ac6f3aa4c957dc096a2ae8f3b10139
Stefan <stefan.gulas@gmail.com>
2021-01-13 05:53:13 +0100

    BigONotation added

---
+++ DataStructureTests/BigONotationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataStructures;
+using NUnit.Framework;
+
+namespace DataStructureTests
+{
+  public class BigONotationTests
+  {
+    public BigONotation bigONotation { get; set; }
+    private List<int> numbers;
+    public List<int> Numbers { get => Numbers; set => numbers = value; }
+    [SetUp]
+    public void Setup()
+    {
+      bigONotation = new BigONotation();
+      numbers = new List<int>();
+      for(int i = 0; i < 1000; i++)
+      {
+        numbers.Add(i);
+      }
+    }
+    [Test]
+    public void TestSimpleArrayLog()
+    {
+      bigONotation.SimpleArrayLog(numbers);
+      Assert.AreEqual(1,1);
+    }
+  }
+}
---
+++ DataStructures/BigONotation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+  public class BigONotation
+  {
+    //Big(
+    public void SimpleArrayLog(List<int> numbers)
+    {
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        Console.WriteLine(numbers[i]);
+      }
+    }
+  }
+}
